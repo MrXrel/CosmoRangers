534939884ce5bf1bee6c05d4f9b17315a92cacb1
FastestDan <danil.skorobogatov_d@mail.ru>
2021-02-03 21:15:10 +0700

    Сделал двух игроков и границы. Оружия нет. Баг: Если Первый Игрок(левый) упрётся в верхнюю стену, он не может лететь назад, пока Второй Игрок(правый) не нажмёт вместе с ним кнопку полёта вниз.

--- VSMode.py
+++ VSMode.py
@@ -1,10 +1,15 @@
+#  Created by X-Corporation
+
+
 import os
 import sys
 
 import pygame
 
 pygame.init()
-FPS = 50
+size = width, height = 900, 500
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("VS Mode Test")
 
 
 def load_image(name, colorkey=None):
@@ -22,147 +27,101 @@ def terminate():
     sys.exit()
 
 
-def start_screen():
-    intro_text = ["Mega Mario Adventure! PLUS! Camera Edition!", "",
-                  "Правила игры",
-                  "Ходи и исследуй мир!",
-                  "Теперь с полным обзором!"]
-
-    fon = pygame.transform.scale(load_image('fon.jpg'), (width, height))
-    screen.blit(fon, (0, 0))
-    font = pygame.font.Font(None, 30)
-    text_coord = 50
-    for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('white'))
-        intro_rect = string_rendered.get_rect()
-        text_coord += 10
-        intro_rect.top = text_coord
-        intro_rect.x = 10
-        text_coord += intro_rect.height
-        screen.blit(string_rendered, intro_rect)
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                terminate()
-            elif event.type == pygame.MOUSEBUTTONDOWN or \
-                    event.type == pygame.KEYDOWN:
-                return  # начинаем игру
-        pygame.display.flip()
-        clock.tick(FPS)
-
-
-def load_level(filename):
-    filename = "data/" + filename
-    # читаем уровень, убирая символы перевода строки
-    with open(filename, 'r') as mapFile:
-        level_map = [line.strip() for line in mapFile]
-
-    # и подсчитываем максимальную длину
-    max_width = max(map(len, level_map))
-
-    # дополняем каждую строку пустыми клетками ('.')
-    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
-
-
-tile_images = {
-    'wall': load_image('box.png'),
-    'empty': load_image('grass.png')
-}
-player_image = load_image('mar.png')
-
-tile_width = tile_height = 50
-
-
-class Tile(pygame.sprite.Sprite):
-    def __init__(self, tile_type, pos_x, pos_y):
-        super().__init__(tiles_group, all_sprites)
-        self.image = tile_images[tile_type]
-        self.rect = self.image.get_rect().move(
-            tile_width * pos_x, tile_height * pos_y)
+all_sprites = pygame.sprite.Group()
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y):
-        super().__init__(player_group, all_sprites)
-        self.image = player_image
-        self.rect = self.image.get_rect().move(
-            tile_width * pos_x + 15, tile_height * pos_y + 5)
-        self.x = pos_x
-        self.y = pos_y
+    def __init__(self, image, x, y, num):
+        super().__init__(all_sprites)
+        self.image = load_image(image)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        if num == 1:
+            self.rect.topleft = self.x, self.y
+        elif num == 2:
+            self.rect.topright = self.x, self.y
+
+    def update(self, x, y, num):
+        self.x = self.x + x
+        self.y = self.y + y
+        if num == 1:
+            self.rect.topleft = self.x, self.y
+            if pygame.sprite.spritecollideany(self, horizontal_borders):
+                self.y = self.y - y
+                self.rect.topleft = self.x, self.y
+            if pygame.sprite.spritecollideany(self, vertical_borders):
+                self.x = self.x - x
+                self.rect.topleft = self.x, self.y
+        elif num == 2:
+            self.rect.topright = self.x, self.y
+            if pygame.sprite.spritecollideany(self, horizontal_borders):
+                self.y = self.y - y
+                self.rect.topright = self.x, self.y
+            if pygame.sprite.spritecollideany(self, vertical_borders):
+                self.x = self.x - x
+                self.rect.topright = self.x, self.y
 
 
-# основной персонаж
-player = None
-# группы спрайтов
-all_sprites = pygame.sprite.Group()
-tiles_group = pygame.sprite.Group()
-player_group = pygame.sprite.Group()
+class Border(pygame.sprite.Sprite):
+    # строго вертикальный или строго горизонтальный отрезок
+    def __init__(self, x1, y1, x2, y2):
+        super().__init__(all_sprites)
+        if x1 == x2:  # вертикальная стенка
+            self.add(vertical_borders)
+            self.image = pygame.Surface([1, y2 - y1])
+            self.rect = pygame.Rect(x1, y1, 1, y2 - y1)
+        else:  # горизонтальная стенка
+            self.add(horizontal_borders)
+            self.image = pygame.Surface([x2 - x1, 1])
+            self.rect = pygame.Rect(x1, y1, x2 - x1, 1)
 
 
-def generate_level(level):
-    new_player, x, y = None, None, None
-    for y in range(len(level)):
-        for x in range(len(level[y])):
-            if level[y][x] == '.':
-                Tile('empty', x, y)
-            elif level[y][x] == '#':
-                Tile('wall', x, y)
-            elif level[y][x] == '@':
-                Tile('empty', x, y)
-                new_player = Player(x, y)
-    # вернем игрока, а также размер поля в клетках
-    return new_player, x, y
-
-
-class Camera:
-    # зададим начальный сдвиг камеры
-    def __init__(self):
-        self.dx = 0
-        self.dy = 0
-
-    # сдвинуть объект obj на смещение камеры
-    def apply(self, obj):
-        obj.rect.x += self.dx
-        obj.rect.y += self.dy
-
-    # позиционировать камеру на объекте target
-    def update(self, target):
-        self.dx = -(target.rect.x + target.rect.w // 2 - width // 2)
-        self.dy = -(target.rect.y + target.rect.h // 2 - height // 2)
-
-
-mname = input()
-fname = os.path.join('data', (mname + '.txt'))
-if not os.path.isfile(fname):
-    print(f"Уровень '{mname}' не найден.")
-    sys.exit()
-mapname = mname + '.txt'
-size = width, height = 500, 500
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Mega Mario Adventure PLUS!: Camera Edition")
-all_sprites = pygame.sprite.Group()
-clock = pygame.time.Clock()
-start_screen()
-player, level_x, level_y = generate_level(load_level(mapname))
-map = load_level(mapname)
-camera = Camera()
+player1 = Player('ship.png', 0, height / 2, 1)
+player2 = Player('ship.png', width, height // 2, 2)
+all_sprites.add(player1)
+all_sprites.add(player2)
+horizontal_borders = pygame.sprite.Group()
+vertical_borders = pygame.sprite.Group()
+Border(0, 0, width, 0)
+Border(0, height, width, height)
+Border(0, 0, 0, height)
+Border(width, 0, width, height)
+Border(width // 2, 0, width // 2, height)
 running = True
+keypress = pygame.key.get_pressed()
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    screen.fill(pygame.Color("black"))
-    tiles_group.draw(screen)
-    player_group.draw(screen)
-    # изменяем ракурс камеры
-    camera.update(player)
-    # обновляем положение всех спрайтов
-    for sprite in all_sprites:
-        camera.apply(sprite)
+    keypress = pygame.key.get_pressed()
+    if keypress[pygame.K_w]:
+        newy = -1
+        player1.update(0, newy, 1)
+    if keypress[pygame.K_s]:
+        y1 = 1
+        player1.update(0, newy, 1)
+    if keypress[pygame.K_d]:
+        newx = 1
+        player1.update(newx, 0, 1)
+    if keypress[pygame.K_a]:
+        newx = -1
+        player1.update(newx, 0, 1)
+    if keypress[pygame.K_UP]:
+        newy = -1
+        player2.update(0, newy, 2)
+    if keypress[pygame.K_DOWN]:
+        newy = 1
+        player2.update(0, newy, 2)
+    if keypress[pygame.K_RIGHT]:
+        newx = 1
+        player2.update(newx, 0, 2)
+    if keypress[pygame.K_LEFT]:
+        newx = -1
+        player2.update(newx, 0, 2)
+    screen.fill(pygame.Color("white"))
+    all_sprites.draw(screen)
     pygame.display.flip()
-    clock.tick(FPS)
 pygame.quit()
 
 pygame.quit()
