2064d1c1dc507b8f9fc53e257602ab9fd82660ef
FastestDan <danil.skorobogatov_d@mail.ru>
2021-02-05 20:43:14 +0700

    Заменил изображения и начал делать пули.       Баг: Средняя стена не отображается и не функционирует.

--- VSMode.py
+++ VSMode.py
@@ -14,7 +14,7 @@ background = pygame.transform.scale(pygame.image.load(os.path.join('Assets', 'ba
 
 
 def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
+    fullname = os.path.join('Assets', name)
     # если файл не существует, то выходим
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
@@ -35,13 +35,21 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, image, x, y, num):
         super().__init__(all_sprites)
         self.image = load_image(image)
-        self.rect = self.image.get_rect()
         self.x = x
         self.y = y
+        self.health = 200
+        self.bullets = []
+        self.damage = 20
+        self.reload = 0
+        self.image = pygame.transform.scale(self.image, [100, 83])
+        self.rect = self.image.get_rect()
         if num == 1:
             self.rect.topleft = self.x, self.y
+            self.laserim = load_image('pixel_laser_yellow.png')
+            self.image = pygame.transform.rotate(self.image, 90)
         elif num == 2:
             self.rect.topright = self.x, self.y
+            self.image = pygame.transform.rotate(self.image, 270)
 
     def update(self, x, y, num):
         self.x = self.x + x
@@ -63,6 +71,26 @@ class Player(pygame.sprite.Sprite):
                 self.x = self.x - x
                 self.rect.topright = self.x, self.y
 
+    def shoot(self):
+        if self.reload == 0:
+            bullet = Bullet(self.x, self.y - self.image.get_height() + 20, self.laserim)
+            self.bullets.append(bullet)
+            self.reload = 1
+
+    def reset_reload(self):
+        # if there is a reload add one every frame
+        if self.reload != 0:
+            self.reload += 1
+        # if reload > 30, it half a second has passed
+        if self.reload > 30:
+            self.reload = 0
+
+    def get_height(self):
+        return self.y
+
+    def get_width(self):
+        return self.x
+
 
 class Border(pygame.sprite.Sprite):
     # строго вертикальный или строго горизонтальный отрезок
@@ -80,11 +108,25 @@ class Border(pygame.sprite.Sprite):
             self.rect = pygame.Rect(x1, y1, x2 - x1, 1)
 
 
+class Bullet:
+    def __init__(self, x, y, img):
+        self.x = x
+        self.y = y
+        self.img = img
+        self.speed = 4
+
+    def draw(self, screen):
+        screen.blit(self.img, (self.x, self.y))
+
+    def move(self):
+        self.y -= self.speed
+
+
 def vs():
     global horizontal_borders
     global vertical_borders
-    player1 = Player('ship.png', 0, height / 2, 1)
-    player2 = Player('ship.png', width, height // 2, 2)
+    player1 = Player('spaceship_yellow.png', 0, height / 2, 1)
+    player2 = Player('spaceship_red.png', width, height // 2, 2)
     all_sprites.add(player1)
     all_sprites.add(player2)
     horizontal_borders = pygame.sprite.Group()
@@ -93,7 +135,8 @@ def vs():
     Border(0, height, width, height)
     Border(0, 0, 0, height)
     Border(width, 0, width, height)
-    Border(width // 2 - 10, 0, width // 2 + 10, height)
+    center = Border(width // 2 - 10, 0, width // 2 + 10, height)
+    all_sprites.add(center)
     clock = pygame.time.Clock()
     player_speed = 3
     running = True
@@ -128,7 +171,6 @@ def vs():
             player2.update(newx, 0, 2)
         screen.blit(background, (0, 0))
         all_sprites.draw(screen)
-
         clock.tick(60)
         pygame.display.flip()
     pygame.quit()
