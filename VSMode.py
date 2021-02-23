379709e309c3e06d960f4b36ace1f40624950473
FastestDan <danil.skorobogatov_d@mail.ru>
2021-02-23 22:40:24 +0700

    Реализовал анимации урона и смерти и добавил спрайтов
    Осталось исправить баг (читай прошлый апдейт)

--- VSMode.py
+++ VSMode.py
@@ -35,21 +35,29 @@ all_sprites = pygame.sprite.Group()
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, image, x, y, num, col, row, width, height):
+    def __init__(self, image, x, y, num, col, row):
         super().__init__(all_sprites)
         self.frames = []
+        self.srect = pygame.Rect(x, y, load_image(image).get_width() // col,
+                                load_image(image).get_height() // row)
         self.sprite(load_image(image), col, row)
         self.spritecur = 0
+        self.imname = image
+        self.row = row
+        self.col = col
         self.image = self.frames[self.spritecur]
-        self.srect = self.srect.move(width, height)
         self.x = x
         self.y = y
         self.num = num
-        self.health = 200
+        self.health = 40
         self.shots = []
         self.damage = 40
         self.cooldown = 0
-        self.image = pygame.transform.scale(self.image, [60, 77])
+        self.revert = False
+        self.count = 0
+        self.trigger = False
+        self.dtrigger = False
+        self.image = pygame.transform.scale(self.image, [80, 114])
         if self.num == 1:
             self.laserim = pygame.transform.rotate(load_image('pixel_laser_yellow.png'), 90)
             self.image = pygame.transform.rotate(self.image, 90)
@@ -63,8 +71,6 @@ class Player(pygame.sprite.Sprite):
         self.mask = pygame.mask.from_surface(self.image)
 
     def sprite(self, image, col, row):
-        self.srect = pygame.Rect(0, 0, image.get_width() // col,
-                                image.get_height() // row)
         for j in range(row):
             for i in range(col):
                 frame = (self.srect.w * i, self.srect.h * j)
@@ -92,9 +98,30 @@ class Player(pygame.sprite.Sprite):
                 self.rect.topright = self.x, self.y
 
     def framechange(self):
-        self.spritecur = (self.spritecur + 1) % len(self.frames)
+        if self.dtrigger and self.spritecur == 0 and self.count != 1:
+            self.count += 1
+        elif self.spritecur == 0 and self.count != 2 and self.trigger:
+            self.revert = True
+            self.count += 1
+        elif self.trigger and self.spritecur == 0 and self.revert and self.count == 2:
+            self.revert = False
+            self.trigger = False
+            self.count = 0
+            self.frames = []
+            self.srect = pygame.Rect(self.x, self.y, load_image(self.imname).get_width() // self.col,
+                                        load_image(self.imname).get_height() // self.row)
+            self.sprite(load_image(self.imname), self.col, self.row)
+            self.image = self.frames[self.spritecur]
+        elif self.dtrigger and self.count == 1 and self.spritecur == (len(self.frames) - 1):
+            self.kill()
         self.image = self.frames[self.spritecur]
-        self.image = pygame.transform.scale(self.image, [60, 77])
+        if not self.dtrigger:
+            self.image = pygame.transform.scale(self.image, [60, 97])
+        else:
+            self.image = pygame.transform.scale(self.image, [141, 150])
+            if self.spritecur == 0:
+                self.rect.center = self.rect.centerx - 20, self.y
+        self.spritecur = (self.spritecur + 1) % len(self.frames)
         if self.num == 1:
             self.image = pygame.transform.rotate(self.image, 90)
         elif self.num == 2:
@@ -121,11 +148,23 @@ class Player(pygame.sprite.Sprite):
     def get_width(self):
         return self.x
 
-    def damage(self):
-        pass
+    def hit(self, image, col, row):
+        self.spritecur = 0
+        self.trigger = True
+        self.frames = []
+        self.srect = pygame.Rect(self.x, self.y, load_image(image).get_width() // col,
+                                 load_image(image).get_height() // row)
+        self.sprite(load_image(image), col, row)
+        self.image = self.frames[self.spritecur]
 
-    def death(self):
-        pass
+    def death(self, image, col, row):
+        self.spritecur = 0
+        self.dtrigger = True
+        self.frames = []
+        self.srect = pygame.Rect(self.x, self.y, load_image(image).get_width() // col,
+                                 load_image(image).get_height() // row)
+        self.sprite(load_image(image), col, row)
+        self.image = self.frames[self.spritecur]
 
 
 class Border(pygame.sprite.Sprite):
@@ -178,8 +217,8 @@ def vs():
                                         (width, height))
     global horizontal_borders
     global vertical_borders
-    player1 = Player('Ships/Flying/BLUEVOYAGERSHEET.png', 25, height // 2 - 50, 1, 4, 1, 40, 57)
-    player2 = Player('Ships/Flying/REDVOYAGERSHEET.png', width - 25, height // 2 - 50, 2, 4, 1, 50, 50)
+    player1 = Player('Ships/Flying/BLUEVOYAGERSHEET.png', 25, height // 2 - 50, 1, 4, 1)
+    player2 = Player('Ships/Flying/REDVOYAGERSHEET.png', width - 25, height // 2 - 50, 2, 4, 1)
     all_sprites.add(player1)
     all_sprites.add(player2)
     horizontal_borders = pygame.sprite.Group()
@@ -237,11 +276,17 @@ def vs():
         player1.framechange()
         player2.framechange()
         all_sprites.draw(screen)
+        p1hp = font.render(f"Здоровье: {player1.health}", False, pygame.Color('white'))
+        p2hp = font.render(f"Здоровье: {player2.health}", False, pygame.Color('white'))
         for shot in player1.shots:
             shot.move()
             if collide(shot, player2):
                 player1.shots.remove(shot)
                 player2.health -= player1.damage
+                if player2.health > 0:
+                    player2.hit('Ships/Damage/REDVOYAGERDAMAGE.png', 4, 1)
+                else:
+                    player2.death('Ships/Death/REDVOYAGERDEATH.png', 4, 3)
             if shot.x - shot.image.get_width() >= width:
                 player1.shots.remove(shot)
         for shot in player2.shots:
@@ -249,6 +294,10 @@ def vs():
             if collide(shot, player1):
                 player2.shots.remove(shot)
                 player1.health -= player2.damage
+                if player1.health > 0:
+                    player1.hit('Ships/Damage/BLUEVOYAGERDAMAGE.png', 4, 1)
+                else:
+                    player1.death('Ships/Death/BLUEVOYAGERDEATH.png', 4, 3)
             if shot.x - shot.image.get_width() == 0:
                 player2.shots.remove(shot)
         player1.reset_reload()
@@ -257,7 +306,7 @@ def vs():
             shot.draw(screen)
         for shot in player2.shots:
             shot.draw(screen)
-        clock.tick(60)
+        clock.tick(30)
         pygame.draw.rect(screen, pygame.Color('white'), (width // 2 - 1, 0, 5, height))
         pygame.display.flip()
     pygame.quit()
