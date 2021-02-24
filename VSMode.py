ed95e091be11ac9fc5b5968469c2f4d79124ea45
FastestDan <danil.skorobogatov_d@mail.ru>
2021-02-24 19:49:09 +0700

    Поменял лазеры на ракеты, дополнил спрайты, сделал шрифт
    Требуется помощь в исправлении бага.
    Баг: Неправильная коллизия ракет и кораблей игроков. Особенно заметна в случае второго игрока

--- VSMode.py
+++ VSMode.py
@@ -49,7 +49,7 @@ class Player(pygame.sprite.Sprite):
         self.x = x
         self.y = y
         self.num = num
-        self.health = 40
+        self.health = 200
         self.shots = []
         self.damage = 40
         self.cooldown = 0
@@ -57,14 +57,14 @@ class Player(pygame.sprite.Sprite):
         self.count = 0
         self.trigger = False
         self.dtrigger = False
-        self.image = pygame.transform.scale(self.image, [80, 114])
+        self.image = pygame.transform.scale(self.image, [55, 114])
         if self.num == 1:
-            self.laserim = pygame.transform.rotate(load_image('pixel_laser_yellow.png'), 90)
+            self.rocketim = 'Projectiles/Rockets/BLUESMALLROCKET.png'
             self.image = pygame.transform.rotate(self.image, 90)
             self.rect = self.image.get_rect()
             self.rect.topleft = self.x, self.y
         elif self.num == 2:
-            self.laserim = pygame.transform.rotate(load_image('pixel_laser_red.png'), 90)
+            self.rocketim = 'Projectiles/Rockets/REDSMALLROCKET.png'
             self.image = pygame.transform.rotate(self.image, 270)
             self.rect = self.image.get_rect()
             self.rect.topright = self.x, self.y
@@ -116,11 +116,11 @@ class Player(pygame.sprite.Sprite):
             self.kill()
         self.image = self.frames[self.spritecur]
         if not self.dtrigger:
-            self.image = pygame.transform.scale(self.image, [60, 97])
+            self.image = pygame.transform.scale(self.image, [55, 97])
         else:
             self.image = pygame.transform.scale(self.image, [141, 150])
             if self.spritecur == 0:
-                self.rect.center = self.rect.centerx - 20, self.y
+                self.rect.center = self.rect.centerx - 20, self.y - 18
         self.spritecur = (self.spritecur + 1) % len(self.frames)
         if self.num == 1:
             self.image = pygame.transform.rotate(self.image, 90)
@@ -130,9 +130,9 @@ class Player(pygame.sprite.Sprite):
     def shoot(self, speed):
         if self.cooldown == 0:
             if self.num == 1:
-                bang = Laser(self.x + 30, self.y - self.image.get_height() + 39, self.laserim, speed)
+                bang = Rocket(self.x + 20, self.y - self.image.get_height() + 72, self.rocketim, speed, 4, 1, 90)
             elif self.num == 2:
-                bang = Laser(self.x - 120, self.y - self.image.get_height() + 39, self.laserim, speed)
+                bang = Rocket(self.x - 100, self.y - self.image.get_height() + 72, self.rocketim, speed, 4, 1, 270)
             self.shots.append(bang)
             self.cooldown = 1
 
@@ -174,21 +174,26 @@ class Border(pygame.sprite.Sprite):
         if x1 == x2:  # вертикальная стенка
             self.add(vertical_borders)
             self.image = pygame.Surface([1, y2 - y1])
-            self.rect = pygame.Rect(x1, y1, 1, y2 - y1)
-            self.image.set_colorkey(pygame.Color('cyan'))
-
         else:  # горизонтальная стенка
             self.add(horizontal_borders)
             self.image = pygame.Surface([x2 - x1, 1])
             self.rect = pygame.Rect(x1, y1, x2 - x1, 1)
 
 
-class Laser:
-    def __init__(self, x, y, img, speed):
+class Rocket:
+    def __init__(self, x, y, img, speed, col, row, degrees):
         self.x = x
         self.y = y
-        self.image = img
+        self.frames = []
+        self.srect = pygame.Rect(x, y, load_image(img).get_width() // col,
+                                load_image(img).get_height() // row)
+        self.sprite(load_image(img), col, row)
+        self.spritecur = 0
+        self.image = self.frames[self.spritecur]
         self.speed = speed
+        self.rotate = degrees
+        self.image = pygame.transform.scale(self.image, [22, 62])
+        self.image = pygame.transform.rotate(self.image, self.rotate)
         self.mask = pygame.mask.from_surface(self.image)
 
     def draw(self, screen):
@@ -196,6 +201,19 @@ class Laser:
 
     def move(self):
         self.x += self.speed
+
+    def sprite(self, image, col, row):
+        for j in range(row):
+            for i in range(col):
+                frame = (self.srect.w * i, self.srect.h * j)
+                self.frames.append(image.subsurface(pygame.Rect(
+                    frame, self.srect.size)))
+
+    def framechange(self):
+        self.image = self.frames[self.spritecur]
+        self.spritecur = (self.spritecur + 1) % len(self.frames)
+        self.image = pygame.transform.scale(self.image, [22, 58])
+        self.image = pygame.transform.rotate(self.image, self.rotate)
 
     def get_height(self):
         return self.y
@@ -210,7 +228,7 @@ def collide(shot, ship) -> bool:
 
 def vs():
     size = width, height = 1000, 600
-    font = pygame.font.SysFont('data/SpaceCur', 45)
+    font = pygame.font.Font('Assets/SpaceFont.ttf', 40)
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption("VS Mode Test")
     background = pygame.transform.scale(pygame.image.load(os.path.join('Assets', 'background-black.png')),
@@ -233,53 +251,49 @@ def vs():
     all_sprites.add(center2)
     clock = pygame.time.Clock()
     player_speed = 3
-    laser_speed = 4
+    rocket_speed = 5
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         keypress = pygame.key.get_pressed()
-        if keypress[pygame.K_w]:
-            newy = -player_speed
-            player1.update(0, newy)
-        if keypress[pygame.K_s]:
-            newy = player_speed
-            player1.update(0, newy)
-        if keypress[pygame.K_d]:
-            newx = player_speed
-            player1.update(newx, 0)
-        if keypress[pygame.K_a]:
-            newx = -player_speed
-            player1.update(newx, 0)
-        if keypress[pygame.K_SPACE]:
-            player1.shoot(laser_speed)
-        if keypress[pygame.K_UP]:
-            newy = -player_speed
-            player2.update(0, newy)
-        if keypress[pygame.K_DOWN]:
-            newy = player_speed
-            player2.update(0, newy)
-        if keypress[pygame.K_RIGHT]:
-            newx = player_speed
-            player2.update(newx, 0)
-        if keypress[pygame.K_LEFT]:
-            newx = -player_speed
-            player2.update(newx, 0)
-        if keypress[pygame.K_RCTRL]:
-            player2.shoot(-laser_speed)
+        if not player1.dtrigger:
+            if keypress[pygame.K_w]:
+                newy = -player_speed
+                player1.update(0, newy)
+            if keypress[pygame.K_s]:
+                newy = player_speed
+                player1.update(0, newy)
+            if keypress[pygame.K_d]:
+                newx = player_speed
+                player1.update(newx, 0)
+            if keypress[pygame.K_a]:
+                newx = -player_speed
+                player1.update(newx, 0)
+            if keypress[pygame.K_SPACE]:
+                player1.shoot(rocket_speed)
+        if not player2.dtrigger:
+            if keypress[pygame.K_UP]:
+                newy = -player_speed
+                player2.update(0, newy)
+            if keypress[pygame.K_DOWN]:
+                newy = player_speed
+                player2.update(0, newy)
+            if keypress[pygame.K_RIGHT]:
+                newx = player_speed
+                player2.update(newx, 0)
+            if keypress[pygame.K_LEFT]:
+                newx = -player_speed
+                player2.update(newx, 0)
+            if keypress[pygame.K_RCTRL]:
+                player2.shoot(-rocket_speed)
         screen.blit(background, (0, 0))
-        p1hp = font.render(f"Здоровье: {player1.health}", False, pygame.Color('white'))
-        p2hp = font.render(f"Здоровье: {player2.health}", False, pygame.Color('white'))
-        screen.blit(p1hp, (25, 10))
-        screen.blit(p2hp, (750, 10))
-        player1.framechange()
-        player2.framechange()
-        all_sprites.draw(screen)
         p1hp = font.render(f"Здоровье: {player1.health}", False, pygame.Color('white'))
         p2hp = font.render(f"Здоровье: {player2.health}", False, pygame.Color('white'))
         for shot in player1.shots:
             shot.move()
+            shot.framechange()
             if collide(shot, player2):
                 player1.shots.remove(shot)
                 player2.health -= player1.damage
@@ -291,6 +305,7 @@ def vs():
                 player1.shots.remove(shot)
         for shot in player2.shots:
             shot.move()
+            shot.framechange()
             if collide(shot, player1):
                 player2.shots.remove(shot)
                 player1.health -= player2.damage
@@ -306,7 +321,12 @@ def vs():
             shot.draw(screen)
         for shot in player2.shots:
             shot.draw(screen)
-        clock.tick(30)
+        player1.framechange()
+        player2.framechange()
+        all_sprites.draw(screen)
+        screen.blit(p1hp, (20, 10))
+        screen.blit(p2hp, (680, 10))
+        clock.tick(60)
         pygame.draw.rect(screen, pygame.Color('white'), (width // 2 - 1, 0, 5, height))
         pygame.display.flip()
     pygame.quit()
