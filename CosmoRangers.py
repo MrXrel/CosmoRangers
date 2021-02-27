d71881201dafb0897a2926beff1a3ee935361029
FastestDan <danil.skorobogatov_d@mail.ru>
2021-02-27 19:45:31 +0700

    Создал прототип главного экрана

---
+++ CosmoRangers.py
@@ -0,0 +1,79 @@
+import arcade
+from arcade.gui import UIManager
+from singleplayer import main
+from VSMode import vs
+
+WIDTH = 700
+HEIGHT = 900
+
+
+class Title(arcade.View):
+    def on_show(self):
+        arcade.set_background_color(arcade.csscolor.BLACK)
+        arcade.set_viewport(0, WIDTH - 1, 0, HEIGHT - 1)
+
+    def on_draw(self):
+        arcade.start_render()
+        arcade.draw_text("CosmoRangers", WIDTH / 2, HEIGHT / 2, arcade.color.CADET_BLUE,
+                         font_size=42, anchor_x='center')
+        arcade.draw_text("Press Enter", WIDTH / 2, HEIGHT / 2 - 50, arcade.color.AMAZON,
+                         font_size=21, anchor_x='center')
+
+    def on_key_press(self, symbol: int, modifiers: int):
+        if symbol == arcade.key.ENTER:
+            scene = Menu()
+            self.window.show_view(scene)
+
+
+class Menu(arcade.View):
+    def __init__(self):
+        super(Menu, self).__init__()
+        self.ui = UIManager(self.window)
+        self.setup()
+
+    def setup(self):
+        self.ui.purge_ui_elements()
+        self.ui.add_ui_element(arcade.gui.UILabel(
+            'MainMenu', center_x=WIDTH / 2, center_y=800,
+        ))
+        arcbtn = arcade.gui.UIFlatButton(
+            "Arcade Mode", center_x=WIDTH // 2, center_y=600, width=400)
+        arcbtn.set_handler("on_click", self.arcstart)
+        self.ui.add_ui_element(arcbtn)
+        vsbtn = arcade.gui.UIFlatButton(
+            "VS Mode", center_x=WIDTH // 2, center_y=400, width=400)
+        vsbtn.set_handler("on_click", self.vsstart)
+        self.ui.add_ui_element(vsbtn)
+        exitbtn = arcade.gui.UIFlatButton(
+            "Exit", center_x=WIDTH // 2, center_y=200, width=400)
+        exitbtn.set_handler("on_click", self.exit)
+        self.ui.add_ui_element(exitbtn)
+
+    def arcstart(self):
+        scene = main()
+        self.window.show_view(scene)
+
+    def vsstart(self):
+        scene = vs()
+        self.window.show_view(scene)
+
+    def exit(self):
+        arcade.close_window()
+
+    def on_show(self):
+        arcade.set_background_color(arcade.csscolor.BLACK)
+        arcade.set_viewport(0, WIDTH - 1, 0, HEIGHT - 1)
+
+    def on_draw(self):
+        arcade.start_render()
+
+
+def start():
+    window = arcade.Window(WIDTH, HEIGHT, "CosmoRangers")
+    scene = Title()
+    window.show_view(scene)
+    arcade.run()
+
+
+if __name__ == "__main__":
+    start()
